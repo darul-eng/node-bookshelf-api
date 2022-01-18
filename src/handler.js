81734ebbe129cf3b062ef10017914934ece6c052
darul-eng <ikhsand18d@student.unhas.ac.id>
2022-01-18 22:44:33 +0800

    Membuat fitur edit dan delete

--- src/handler.js
+++ src/handler.js
@@ -93,10 +93,87 @@ const getBookByIdHandler = (request, h) => {
     return response;
 };
 const editBookByIdHandler = (request, h) => {
-//
+    const { id } = request.params;
+
+    const index = books.findIndex((book) => book.id === id);
+
+    const { name, year, author, summary, publisher, pageCount, readPage, reading } = request.payload;
+    const updatedAt = new Date().toISOString();
+    let finished = false;
+    if (pageCount === readPage){
+        finished = true;
+    }
+//bila nama kosong
+    if (name == undefined || name === ''){
+        const response = h.response({
+            status: 'fail',
+            message: 'Gagal menambahkan buku. Mohon isi nama buku',
+        });
+        response.code(400);
+        return response;
+    }
+    //ketika readpage > pagecount
+    if ( readPage > pageCount) {
+        const response = h.response({
+            status: 'fail',
+            message: 'Gagal menambahkan buku. readPage tidak boleh lebih besar dari pageCount'
+        });
+        response.code(400);
+        return response;
+    }
+
+    if (index !== -1){
+        books[index] = {
+            ...books[index],
+            name,
+            year,
+            author,
+            summary,
+            publisher,
+            pageCount,
+            readPage,
+            reading,
+            finished,
+            updatedAt
+        };
+
+        const response = h.response({
+            status: 'success',
+            message: 'Buku berhasil diperbarui'
+        });
+        response.code(200);
+        return response;
+    }
+
+    const response = h.response({
+        status: 'fail',
+        message: 'Gagal memperbarui buku. Id tidak ditemukan',
+    });
+    response.code(404);
+    return response;
+
 };
 const deleteBookByIdHandler = (request, h) => {
-//
+    const { id } = request.params;
+
+    const index = books.findIndex((book) => book.id === id);
+
+    if (index !== -1){
+        books.splice(index, 1);
+        const response = h.response({
+            status: 'success',
+            message: 'Buku berhasil dihapus'
+        });
+        response.code(200);
+        return response;
+    }
+
+    const response = h.response({
+        status: 'fail',
+        message: 'Buku gagal dihapus. Id tidak ditemukan'
+    });
+    response.code(404);
+    return response;
 };
 
 module.exports = {addBookHandler, getAllBooksHandler, getBookByIdHandler, editBookByIdHandler, deleteBookByIdHandler};
